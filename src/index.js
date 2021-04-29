368d7f15040d8d835d0b661910e76377a80c4ebd
Brian <Brian.Rui01@gmail.com>
2021-04-29 19:49:02 -0400

    Add time travel

--- src/index.js
+++ src/index.js
@@ -12,59 +12,20 @@ function Square(props) {
 }
   
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: new Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
 
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-
-    // Return early if there is a winner or if square is filled
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    });
-  }
-
-  handleResetRequest() {
-    const squares = this.state.squares.slice();
-    for (let i = 0; i < squares.length; i++ ) {
-      squares[i] = null;
-    }
-    this.setState({
-      squares: squares,
-      xIsNext: true
-    });
-  }
 
   renderSquare(i) {
     return (
-      <Square value={this.state.squares[i]} 
-      onClick={() => this.handleClick(i)}
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -80,24 +41,98 @@ class Board extends React.Component {
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
-        <div className="reset">
-          <button onClick={() => this.handleResetRequest()} className="resetButton">RESET</button>
-        </div>
+
     </div>
     );
   }
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history:[{
+        squares: Array(9).fill(null),
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+    };
+  }
+
+  handleResetRequest() {
+    // const squares = this.state.squares.slice();
+    // for (let i = 0; i < squares.length; i++ ) {
+    //   squares[i] = null;
+    // }
+    // this.setState({
+    //   squares: squares,
+    //   xIsNext: true,
+    // });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+  
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    // Return early if there is a winner or if square is filled
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const description = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{description}</button>
+        </li>
+      )
+    });
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
+          <div className="reset">
+          <button onClick={() => this.handleResetRequest()} className="resetButton">RESET</button>
+          </div>
         </div>
       </div>
     );
